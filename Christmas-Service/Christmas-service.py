5721ffcf1b615c602653a254c41480af4e1e5f9a
EnvillePlease <clark@cnsoft-online.com>
2021-12-07 11:01:35 +0000

    Looping added

--- Christmas-Service/Christmas-service.py
+++ Christmas-Service/Christmas-service.py
@@ -6,35 +6,37 @@ star = Star(pwm=True)
 # Twinkle Pattern
 leds = star.leds
 
-twinkle_loop=300
-while twinkle_loop<0:
-    for led in leds:
-        led.pulse()
-        sleep(.2)
-        twinkle_loop-=1
-
-# Slow Walk
-step = 0.5
-count = 0
 while True:
-    if(count%26!=0):
-        leds[count%26].on()
-        sleep(step)
-        leds[count%26].off()
-            
-    count += 1
-    step = step*0.99
-        
-    if(step <= 0.0001):
-        star.inner.blink(on_time=0.5,off_time=0.5,n=5)
-        sleep(5)
-        count = 0
-        step = 2
+    twinkle_loop=300
+    while twinkle_loop>0:
+        for led in leds:
+            led.pulse()
+            sleep(.2)
+            twinkle_loop-=1
 
- #In Out
-in_out_loop=120
-star.inner.on()
-while in_out_loop<0:
-    star.toggle()
-    sleep(.5)
-    in_out_loop-=1
+    # Slow Walk
+    step = 0.5
+    count = 0
+    while True:
+        if(count%26!=0):
+            leds[count%26].on()
+            sleep(step)
+            leds[count%26].off()
+
+        count += 1
+        step = step*0.99
+
+        if(step <= 0.0001):
+            star.inner.blink(on_time=0.5,off_time=0.5,n=5)
+            sleep(5)
+            count = 0
+            step = 2
+            break
+
+     #In Out
+    in_out_loop=120
+    star.inner.on()
+    while in_out_loop>0:
+        star.toggle()
+        sleep(.5)
+        in_out_loop-=1
